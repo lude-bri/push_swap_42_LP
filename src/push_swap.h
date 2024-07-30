d08548241df0604f45fcf3c85d5a7b52f3cc9c3e
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-07-30 09:44:35 +0100

    push_swap

---
+++ src/push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 08:56:09 by luigi             #+#    #+#             */
+/*   Updated: 2024/07/30 08:56:20 by luigi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//create double linked list
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <string.h>
+
+typedef struct s_list
+{
+	int				num;
+	size_t			rank;
+	struct s_list	*prev;
+	struct s_list	*next;
+}	t_stack;	
+
+
+#endif
