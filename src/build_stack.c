b7fe1ca65b0e502799ca0ca434ed69a38d812c8e
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-07 10:32:13 +0100

    push_swap

--- src/build_stack.c
+++ src/build_stack.c
@@ -1,46 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   build_stack.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/07 09:28:58 by luigi             #+#    #+#             */
-/*   Updated: 2024/08/07 09:52:58 by luigi            ###   ########.fr       */
+/*   Created: 2024/08/07 10:24:31 by luigi             #+#    #+#             */
+/*   Updated: 2024/08/07 10:32:10 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-//1. parse
-//2. create stack
-//3. resolve push_swap
-
-int	main(int ac, char **av)
+t_stack *create_stack(char **str)
 {
-	char		**numbers;
 	t_stack		*stack;
+	const int	count;
+	int			i;
 
-	if (ac == 1)
-		exit(0);
-	if (ac == 2)
-	{
-		numbers = ft_split(av[1], ' ');
-		if (!*numbers)
-			exit(0);
-		stack = create_stack(numbers);
-		free_str(numbers);
-	}
-	else
-	{
-		numbers = av + 1;
-		stack = create_stack(numbers);
-	}
+	count = number_sanity_check(str);
+	if (count == 0)
+		return (NULL);
+	stack = malloc(sizeof(t_stack));
 	if (!stack)
-	{
-		ft_putstr_fd("Error creating stack\n", 0);
-		exit(1);
-	}
-	push_swap(stack);
-	free_stack(stack);
+		return (NULL);
+	stack->num = malloc(sizeof(int) * count);
+	if (!stack->num)
+		return (free(stack), NULL);
+	i = count;
+	while (i >= 0)
+
 }
