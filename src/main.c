d5272ba2ebefa8579668c17f3cfe9d366ded89d1
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-07 09:35:23 +0100

    push_swap

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 09:28:58 by luigi             #+#    #+#             */
-/*   Updated: 2024/08/07 09:29:45 by luigi            ###   ########.fr       */
+/*   Updated: 2024/08/07 09:35:20 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,28 @@
 
 
 
+
+int	main(int ac, char **av)
+{
+	char		**numbers;
+	t_stack		*stack;
+
+	if (ac == 1)
+		exit (0);
+	if (ac == 2)
+	{
+		//parsing
+	}
+	else
+	{
+		numbers = av + 1;
+		stack = create_stack(numbers);
+	}
+	if (!stack)
+	{
+		ft_putstr_fd("Error creating stack\n", 0);
+		exit(1);
+	}
+	push_swap(stack);
+	delete_stack(stack);
+}
