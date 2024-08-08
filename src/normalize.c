146eda098efaddee154766257b15cf8b7896f62d
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-08 11:41:07 +0100

    push_swap

---
+++ src/normalize.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   normalize.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 11:25:35 by luigi             #+#    #+#             */
+/*   Updated: 2024/08/08 11:41:03 by luigi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	to_rank(int num, int size)
+{
+	t_stack		*sorted_list;
+
+	sorted_list = malloc(sizeof(t_stack) * size);
+	if (!sorted_list)
+		return (NULL);
+	i = -1;
+	while (++i < size)
+	{
+	
+	}
+}
