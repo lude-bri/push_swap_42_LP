704ccf8ca29424d758762c6a993955f3c069ffb4
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-23 12:22:05 +0100

    push_swap

--- .build/main.d
+++
@@ -1,5 +0,0 @@
-.build/main.o: src/main.c src/push_swap.h src/../lib/libft_42/libft.h
-
-src/push_swap.h:
-
-src/../lib/libft_42/libft.h:
--- .build/sanity_check.d
+++
@@ -1,6 +0,0 @@
-.build/sanity_check.o: src/sanity_check.c src/push_swap.h \
-  src/../lib/libft_42/libft.h
-
-src/push_swap.h:
-
-src/../lib/libft_42/libft.h:
