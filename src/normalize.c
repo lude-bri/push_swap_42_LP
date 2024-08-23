d1539b132cabb1f146e728d14453e0037b4392e6
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-23 13:20:37 +0100

    push_swap

--- src/normalize.c
+++ src/normalize.c
@@ -59,10 +59,11 @@ int to_rank(t_stack *stack)
 
 void quick_sort_stack(t_stack **stack, int low, int high)
 {
+	int	pi;
+
+	pi = partition_stack(stack, low, high);
     if (low < high)
     {
-        int pi = partition_stack(stack, low, high);
-
         quick_sort_stack(stack, low, pi - 1);
         quick_sort_stack(stack, pi + 1, high);
     }
@@ -70,18 +71,25 @@ void quick_sort_stack(t_stack **stack, int low, int high)
 
 int partition_stack(t_stack **stack, int low, int high)
 {
-    t_stack *pivot_node = get_node_at_index(*stack, high);
-    int pivot = pivot_node->num;
-    int i = low - 1;
+    t_stack *pivot_node;
+	t_stack	*current;
+    int pivot;
+    int i;
+	int j;
 
-    for (int j = low; j < high; j++)
+	pivot_node = get_node_at_index(*stack, high);
+	pivot = pivot_node->num;
+	i = low - 1;
+	j = low;
+    while (j < high)
     {
-        t_stack *current = get_node_at_index(*stack, j);
+        current = get_node_at_index(*stack, j);
         if (current->num <= pivot)
         {
             i++;
             swap_nodes(stack, i, j);
         }
+        j++;
     }
     swap_nodes(stack, i + 1, high);
     return (i + 1);
@@ -89,53 +97,30 @@ int partition_stack(t_stack **stack, int low, int high)
 
 t_stack *get_node_at_index(t_stack *stack, int index)
 {
-    t_stack *current = stack;
-    for (int i = 0; i < index; i++)
+    t_stack *current;
+    int i;
+
+	current = stack;
+	i = 0;
+    while (i < index)
+    {
         current = current->next;
-    return current;
+        i++;
+    }
+    return (current);
 }
 
 void swap_nodes(t_stack **stack, int i, int j)
 {
-    t_stack *node_i = get_node_at_index(*stack, i);
-    t_stack *node_j = get_node_at_index(*stack, j);
+    t_stack *node_i;
+	t_stack *node_j;
+	int	tmp;
 
-    int temp = node_i->num;
+	node_i = get_node_at_index(*stack, i);
+	node_j = get_node_at_index(*stack, i);
+    
+	tmp = node_i->num;
     node_i->num = node_j->num;
-    node_j->num = temp;
-}
-
-int duplicate_check_stack(t_stack *stack, int size)
-{
-    t_stack *current = stack;
-    for (int i = 0; i < size; i++)
-    {
-        t_stack *runner = current->next;
-        for (int j = i + 1; j < size; j++)
-        {
-            if (current->num == runner->num)
-                return (0);
-            runner = runner->next;
-        }
-        current = current->next;
-    }
-    return (1);
-}
-
-int binary_search_stack(t_stack *stack, int target, int low, int high)
-{
-    while (low <= high)
-    {
-        int mid = low + (high - low) / 2;
-        t_stack *mid_node = get_node_at_index(stack, mid);
-
-        if (mid_node->num == target)
-            return mid;
-        else if (mid_node->num < target)
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return -1;
+    node_j->num = tmp;
 }
 
