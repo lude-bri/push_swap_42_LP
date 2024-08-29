71458c74c3646eb7821e496aac90485a43d65d59
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-29 11:04:07 +0100

    push_swap

--- src/push_operations.c
+++ src/push_operations.c
@@ -6,37 +6,37 @@
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:07:26 by luigi             #+#    #+#             */
-/*   Updated: 2024/08/29 10:08:02 by luigi            ###   ########.fr       */
+/*   Updated: 2024/08/29 11:04:00 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 //pa (push a) - Take the first element at the top of b and put it at the top of a. Do nothing if b is empty.
-void	pa(t_stack *stack_a, t_stack *stack_b)
+void	pa(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack		*current;
 
-	if (b_size(stack_b) != 0)
+	if (b_size(*stack_b) != 0)
 	{
-		current = stack_b;
-		stack_b = stack_b->next;
-		current->next = stack_a;
-		stack_a = current;
+		current = *stack_b;
+		*stack_b = (*stack_b)->next;
+		current->next = *stack_a;
+		*stack_a = current;
 	}
 }
 
 //pb (push b) - Take the first element at the top of a and put it at the top of b. Do nothing if a is empty.
-void	pb(t_stack *stack_a, t_stack *stack_b)
+void	pb(t_stack **stack_a, t_stack **stack_b)
 {
 	t_stack		*current;
 
-	if (a_size(stack_a) != 0)
+	if (a_size(*stack_a) != 0)
 	{
-		current = stack_a;
-		stack_a = stack_b->next;
-		current->next = stack_b;
-		stack_b = current;
+		current = *stack_a;
+		*stack_a = (*stack_a)->next;
+		current->next = *stack_b;
+		*stack_b = current;
 	}
 }
 
