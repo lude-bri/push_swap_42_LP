add1bb78df8ab12aed687e7245c857dc1482426f
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-29 10:57:12 +0100

    push_swap

--- src/rotate_operations.c
+++ src/rotate_operations.c
@@ -6,40 +6,40 @@
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:08:52 by luigi             #+#    #+#             */
-/*   Updated: 2024/08/29 10:09:19 by luigi            ###   ########.fr       */
+/*   Updated: 2024/08/29 10:57:07 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 //ra (rotate a) - Shift up all elements of stack a by 1. The first elements becomes the last one.
-void	ra(t_stack *stack)
+void	ra(t_stack **stack)
 {
 	t_stack	*last_node;
 	t_stack *current;
 
-	current = stack;
-	last_node = find_lastnode(stack);
-	stack = stack->next;
+	current = *stack;
+	last_node = find_lastnode(*stack);
+	*stack = (*stack)->next;
 	last_node->next = current;
-	last_node->next->next = stack;
+	last_node->next->next = *stack;
 }
 
 //rb (rotate b) - Shift up all elements of stack b by 1. The first element becomes the last one.
-void	rb(t_stack *stack)
+void	rb(t_stack **stack)
 {
 	t_stack	*last_node;
 	t_stack *current;
 
-	current = stack;
-	last_node = find_lastnode(stack);
-	stack = stack->next;
+	current = *stack;
+	last_node = find_lastnode(*stack);
+	*stack = (*stack)->next;
 	last_node->next = current;
-	last_node->next->next = stack;
+	last_node->next->next = *stack;
 }
 
 //rr (rotate a and rotate b) - Do ra and rb
-void	rr(t_stack *stack)
+void	rr(t_stack **stack)
 {
 	ra(stack);
 	rb(stack);
