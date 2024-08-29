151b9379b54dd90e41ea64fde762a976be43b69e
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-08-29 11:02:09 +0100

    push_swap

--- src/swap_operations.c
+++ src/swap_operations.c
@@ -6,41 +6,40 @@
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 10:05:49 by luigi             #+#    #+#             */
-/*   Updated: 2024/08/29 10:06:42 by luigi            ###   ########.fr       */
+/*   Updated: 2024/08/29 11:01:54 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 //sa (swap a) - Swap the 2 first elements at the top of stack a. Do nothing if there is only one or no elements.
-void	sa(t_stack *stack)
+void	sa(t_stack **stack)
 {
-	int	tmp;
+	int		tmp;
 
-	if (stack->size > 1)
+	if ((*stack)->size > 1)
 	{
-		tmp = stack->num;
-		stack->num = stack->next->num;
-		stack->next->num = tmp;
-		stack->next->num = tmp;
+		tmp = (*stack)->num;
+		(*stack)->num = (*stack)->next->num;
+		(*stack)->next->num = tmp;
 	}
 }
 
 //sb (swap b) - Swap the 2 first elements at the top of stack b. Do nothing if there is only one or no elements.
-void	sb(t_stack *stack)
+void	sb(t_stack **stack)
 {
-	int	tmp;
+	int		tmp;
 
-	if (stack->size > 1)
+	if ((*stack)->size > 1)
 	{
-		tmp = stack->num;
-		stack->num = stack->next->num;
-		stack->next->num = tmp;
+		tmp = (*stack)->num;
+		(*stack)->num = (*stack)->next->num;
+		(*stack)->next->num = tmp;
 	}
 }
 
 //ss (swap a and swap b) - do sa and sb at the same time
-void	ss(t_stack *stack)
+void	ss(t_stack **stack)
 {
 	sa(stack);
 	sb(stack);
